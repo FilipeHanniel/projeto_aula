f67f5da4f2418566dc7730be3b18003955e62131
Filipe Hanniel - Base <filipehanniel@gmail.com>
2023-05-23 12:29:50 -0300

    Inicio do JogoCarta

--- src/array/JogoCarta.java
+++ src/array/JogoCarta.java
@@ -2,7 +2,11 @@ package array;
 
 public class JogoCarta {
     public static void main(String[] args) {
-        
+        String[] nipes = {"Ouros", "Copas", "Espadas", "Paus"};
+        String[] faces = {"Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei"};
+        // as linhas abaixo recuperam de forma aleatória o conteúdo dos arrays
+        String[] nipe = nipes[(int) Math.random() * 4];
+        String[] face = faces[(int) Math.random() * faces.length];
     }
     
 }
