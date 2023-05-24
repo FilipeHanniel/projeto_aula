1fb622f32f22111fed92a9bdc352864e2674433a
Filipe Hanniel <filipehanniel@gmail.com>
2023-05-24 07:47:11 -0300

    Jogo Cartas

--- src/array/JogoCarta.java
+++ src/array/JogoCarta.java
@@ -5,8 +5,10 @@ public class JogoCarta {
         String[] nipes = {"Ouros", "Copas", "Espadas", "Paus"};
         String[] faces = {"Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei"};
         // as linhas abaixo recuperam de forma aleatória o conteúdo dos arrays
-        String[] nipe = nipes[(int) Math.random() * 4];
-        String[] face = faces[(int) Math.random() * faces.length];
+        String nipe = nipes[(int) (Math.random() * 4)];
+        String face = faces[(int) (Math.random() * faces.length)];
+        // a linha abaixo exibe a carta sorteada
+        System.out.println(face + " de " + nipe);
     }
     
 }
